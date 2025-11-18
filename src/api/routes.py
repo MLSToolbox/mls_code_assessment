1b7783dd25739689823c9fa192c7098c05e517f5
beto <betooxx.dev@gmail.com>
2025-11-18 21:58:14 +0100

    refactor: move validation to api module and remove utils folder

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,10 +4,10 @@ from pydantic import ValidationError as PydanticValidationError
 
 from api.serializers import ResponseSerializer
 from api.services import UploadService, AnalysisService
+from api.validation import validate_zip_file
 from analyzers.pipeline.pipeline_overrides import AnalysisRequest
 from core.exceptions import ValidationError
 import config.settings as config
-from utils.validation import validate_zip_file
 
 
 def create_routes(app: Flask) -> Flask:
